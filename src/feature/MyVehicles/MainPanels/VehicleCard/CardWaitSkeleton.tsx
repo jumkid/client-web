b08a71803da14779a07b718d297ee3b8f6645cbc
chooli <chooli.yip@gmail.com>
2023-09-08 14:18:10 -0400

    keep access token alive when user browser remains open

--- src/feature/MyVehicles/MainPanels/VehicleCard/CardWaitSkeleton.tsx
+++ src/feature/MyVehicles/MainPanels/VehicleCard/CardWaitSkeleton.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Skeleton, Box } from '@mui/material';
-import { useAppSelector } from '../../../../App.hooks';
-import { RootState } from '../../../../store';
-import * as _ from 'lodash';
 import './index.css';
 
 type Props = {
@@ -10,14 +7,12 @@ type Props = {
 }
 
 function CardWaitSkeleton ({isShown}:Props) {
-  const matchVehicles = useAppSelector((state:RootState) => state.searchVehicles.matchVehicles);
-  const count = matchVehicles.length < 1 ? 1 : matchVehicles.length;
-
   return (
     <>
-      { isShown && _.times(count, (i) => (
-        <Skeleton key={i} variant="rectangular" width="366px" height="240px"/>
-      ))
+      { isShown &&
+        <Box>
+          <Skeleton variant="rectangular" width="366px" height="240px"/>
+        </Box>
       }
     </>
   )
